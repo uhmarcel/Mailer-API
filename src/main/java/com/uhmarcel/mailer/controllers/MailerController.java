8012f6c9aca668e9c63af8af3c9f85c432c25acf
Marcel Riera <mrier011@fiu.edu>
2020-10-18 01:24:18 -0400

    Updated controller endpoint response

--- src/main/java/com/uhmarcel/mailer/controllers/MailerController.java
+++ src/main/java/com/uhmarcel/mailer/controllers/MailerController.java
@@ -2,12 +2,9 @@ package com.uhmarcel.mailer.controllers;
 
 import com.uhmarcel.mailer.models.Email;
 import com.uhmarcel.mailer.services.MailerService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.LinkedHashMap;
 
 @RestController
 public class MailerController {
@@ -19,8 +16,9 @@ public class MailerController {
     }
 
     @PostMapping("/send")
-    public void sendEmail(@RequestBody Email email) {
+    public Email sendEmail(@RequestBody Email email) {
         this.mailerService.sendMail(email);
+        return email;
     }
 
 }
